4990e06449fca66877542a6a9164913ef5c3e1a3
EgnaroDev2 <47803678+EgnaroDev2@users.noreply.github.com>
2020-09-24 15:02:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf500
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-}
+$(document).ready(function() {
+    $("html").on("click", function() {
+      var randR = Math.floor(Math.random() * 256) + ", ";
+      var randG = Math.floor(Math.random() * 256) + ", ";
+      var randB = Math.floor(Math.random() * 256);
+  
+      var randomRgb = "rgb(" + randR + randG + randB + ")";
+      // .res -> body
+      //$(".res").html(randomRgb);
+      $("body").css("background-color", randomRgb);
+      $("body").css("color", "white");
+    });
+  });
